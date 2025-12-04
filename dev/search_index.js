47f0dd5e647fb6024a50d62957896fac7b3b0953
Documenter.jl <documenter@juliadocs.github.io>
2025-12-04 14:17:46 +0000

    build based on e8edfdd

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#RobustNMF","page":"Home","title":"RobustNMF","text":"Documentation for RobustNMF.\n\n","category":"section"}]
+[{"location":"#RobustNMF","page":"Home","title":"RobustNMF","text":"Documentation for RobustNMF.\n\n","category":"section"},{"location":"#RobustNMF.timestwo-Tuple{Any}","page":"Home","title":"RobustNMF.timestwo","text":"timestwo(x)\n\nMultiply the input x by two.\n\n\n\n\n\n","category":"method"}]
 }
